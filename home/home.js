92f7df95622ba2cc9eaf6a4d55f1e39cf9d425d4
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-02-28 16:18:57 -0600

    Add home page

---
+++ home/home.js
@@ -0,0 +1,10 @@
+var container = $('#grid-wrapper');
+// $container.imagesLoaded(function(){
+  var msnry = new  Masonry(container[0], {
+    itemSelector : '.item',
+    columnWidth : 234,
+    isFitWidth: true,
+    gutterWidth: 10,
+    transitionDuration: '0.4s',
+  });
+// });
